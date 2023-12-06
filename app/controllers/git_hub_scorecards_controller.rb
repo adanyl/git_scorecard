9b4d3e897b5a26a2755ab1c0ad24a5fabd8caf0d
Oleksii Danylenko <alexey.danylenko@gmail.com>
2023-12-06 12:12:48 +0200

    [Feature] add filter of events

--- app/controllers/git_hub_scorecards_controller.rb
+++ app/controllers/git_hub_scorecards_controller.rb
@@ -4,13 +4,13 @@ class GitHubScorecardsController < ApplicationController
   end
 
   def update_score
-    one_week_ago = Time.now - 7.days
+    one_week_ago = Date.today - 7.days
 
     events = octokit.repository_events('git/git', per_page: 100)
 
-    fitered_events = events.select { |event| event.created_at > one_week_ago }
+    filtered_events = events.select { |event| event.created_at > one_week_ago }
 
-    fitered_events.each do |event|
+    filtered_events.each do |event|
       GitHubScorecard.update_score(event['actor']['login'], event['type'], event['id'])
     end
 
